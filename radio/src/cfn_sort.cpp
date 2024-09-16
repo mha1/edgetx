8b16491f72c12d43ba6fec9061ce1f8c569c0691
Michael <101.37584@germanynet.de>
2024-09-16 15:23:03 +0200

    only necessary for local builds

--- radio/src/cfn_sort.cpp
+++ radio/src/cfn_sort.cpp
@@ -791,6 +791,7 @@ Functions cfn_sorted[] = {
   /* SD лог */ FUNC_LOGS,
 #else
   /* Adjust */ FUNC_ADJUST_GVAR,
+  /* Arm */ FUNC_ARM,
   /* Audio Amp Off */ FUNC_DISABLE_AUDIO_AMP,
 #if !defined(OLED_SCREEN)
   /* Backlight */ FUNC_BACKLIGHT,
