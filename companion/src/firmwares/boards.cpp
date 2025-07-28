1098e6c4c73c372a4fa88c3af33f7ee0facb76a4
philmoz <phil.a.mitchell@gmail.com>
2025-07-28 18:52:37 +1000

    Fix rebase.

--- companion/src/firmwares/boards.cpp
+++ companion/src/firmwares/boards.cpp
@@ -416,7 +416,7 @@ int Boards::getCapability(Board::Type board, Board::Capability capability)
       return IS_RADIOMASTER_MT12(board);
 
     case FunctionSwitchColors:
-      return IS_RADIOMASTER_GX12(board) || IS_FLYSKY_ST16(board);
+      return IS_RADIOMASTER_GX12(board) || IS_FLYSKY_ST16(board) || IS_RADIOMASTER_TX15(board);
 
     default:
       return getBoardJson(board)->getCapability(capability);
